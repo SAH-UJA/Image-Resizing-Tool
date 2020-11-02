d9faf4057357074c4b9945ba8de2c54889120195
SAH-UJA <53290539+SAH-UJA@users.noreply.github.com>
2020-11-02 20:36:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,15 +13,15 @@ def main():
 	img = streamlit.file_uploader("Upload your image", type=['jpg'])
 	if img:
 		im = Image.open(img)
-		im.save('buff.jpeg')
+		im.save('buff.jpg')
 		width = streamlit.slider("width", min_value=100, max_value=1000, step=10)
 		height = streamlit.slider("height", min_value=100, max_value=1000, step=10)
-		im = cv2.resize(cv2.imread('buff.jpeg'), (width,height))
+		im = cv2.resize(cv2.imread('buff.jpg'), (width,height))
 		streamlit.image(im)
-		cv2.imwrite('graph.jpeg', im)
-		with open('graph.jpeg','rb') as f:
+		cv2.imwrite('graph.jpg', im)
+		with open('graph.jpg','rb') as f:
 			enc = base64.b64encode(f.read()).decode()
-			html = f'<a href="data:file/jpeg;base64,{enc}" download="img.jpeg">Download</a>'
+			html = f'<a href="data:image/jpg;base64,{enc}" download="img.jpg">Download</a>'
 			streamlit.markdown(html, unsafe_allow_html=True)
 if __name__=='__main__':
     main()
