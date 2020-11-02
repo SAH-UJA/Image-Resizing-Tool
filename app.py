689717f530eb9f2d118fec06ca0c4aa59f08f040
SAH-UJA <53290539+SAH-UJA@users.noreply.github.com>
2020-11-02 21:13:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,21 +4,21 @@ import cv2
 from PIL import Image
 
 def main():
-	streamlit.markdown("""# Welcome to Image Resizing Tool""")
-	banner = cv2.imread('banner.jpg')
-        streamlit.image(banner)
-	img = streamlit.file_uploader("Upload your image", type=['jpg'])
-	if img:
-		im = Image.open(img)
-		im.save('buff.jpg')
-		width = streamlit.slider("width", min_value=100, max_value=1000, step=10)
-		height = streamlit.slider("height", min_value=100, max_value=1000, step=10)
-		im = cv2.resize(cv2.imread('buff.jpg'), (width,height))
-		streamlit.image(im)
-		cv2.imwrite('graph.jpg', im)
-		with open('graph.jpg','rb') as f:
-			enc = base64.b64encode(f.read()).decode()
-			html = f'<a href="data:image/jpg;base64,{enc}" download="img.jpg">Download</a>'
-			streamlit.markdown(html, unsafe_allow_html=True)
+    streamlit.markdown("""# Welcome to Image Resizing Tool""")
+    banner = cv2.imread('banner.jpg')
+    streamlit.image(banner)
+    img = streamlit.file_uploader("Upload your image", type=['jpg'])
+    if img:
+        im = Image.open(img)
+        im.save('buff.jpg')
+        width = streamlit.slider("width", min_value=100, max_value=1000, step=10)
+        height = streamlit.slider("height", min_value=100, max_value=1000, step=10)
+        im = cv2.resize(cv2.imread('buff.jpg'), (width,height))
+        streamlit.image(im)
+        cv2.imwrite('graph.jpg', im)
+        with open('graph.jpg','rb') as f:
+            enc = base64.b64encode(f.read()).decode()
+            html = f'<a href="data:image/jpg;base64,{enc}" download="img.jpg">Download</a>'
+            streamlit.markdown(html, unsafe_allow_html=True)
 if __name__=='__main__':
     main()
